61b584c8b8b34ef3137ceffe0740fa8d08d09a4c
Adam Featherston <j.adam.featherston@gmail.com>
2022-10-06 16:03:44 -0500

    PUT method

--- views/animal_requests.py
+++ views/animal_requests.py
@@ -1,45 +1,17 @@
 ANIMALS = [
-    {
-        "id": 1,
-        "name": "Snickers",
-        "species": "Dog",
-        "locationId": 1,
-        "customerId": 4
-    },
-    {
-        "id": 2,
-        "name": "Roman",
-        "species": "Dog",
-        "locationId": 1,
-        "customerId": 2
-    },
-    {
-        "id": 3,
-        "name": "Blue",
-        "species": "Cat",
-        "locationId": 2,
-        "customerId": 1
-    },
-    {
-        "id": 4,
-        "name": "Thor",
-        "species": "Dog",
-        "locationId": 2,
-        "customerId": 3
-    },
-    {
-        "id": 5,
-        "name": "Rocco",
-        "species": "Dog",
-        "locationId": 2,
-        "customerId": 3
-    }
+    {"id": 1, "name": "Snickers", "species": "Dog", "locationId": 1, "customerId": 4, "status": "Admitted"},
+    {"id": 2, "name": "Roman", "species": "Dog", "locationId": 1, "customerId": 2, "status": "Admitted"},
+    {"id": 3, "name": "Blue", "species": "Cat", "locationId": 2, "customerId": 1, "status": "Admitted"},
+    {"id": 4, "name": "Thor", "species": "Dog", "locationId": 2, "customerId": 3, "status": "Admitted"},
+    {"id": 5, "name": "Rocco", "species": "Dog", "locationId": 2, "customerId": 3, "status": "Admitted"},
 ]
 
-def get_all_animals():
-        return ANIMALS
 
-    # Function with a single parameter
+def get_all_animals():
+    return ANIMALS
+
+
+# Function with a single parameter
 def get_single_animal(id):
     # Variable to hold the found animal, if it exists
     requested_animal = None
@@ -53,6 +25,7 @@ def get_single_animal(id):
             requested_animal = animal
 
     return requested_animal
+
 
 def create_animal(animal):
     # Get the id value of the last animal in the list
@@ -69,3 +42,28 @@ def create_animal(animal):
 
     # Return the dictionary with `id` property added
     return animal
+
+
+def delete_animal(id):
+    # Initial -1 value for animal index, in case one isn't found
+    animal_index = -1
+
+    # Iterate the ANIMALS list, but use enumerate() so that you
+    # can access the index value of each item
+    for index, animal in enumerate(ANIMALS):
+        if animal["id"] == id:
+            # Found the animal. Store the current index.
+            animal_index = index
+
+    # If the animal was found, use pop(int) to remove it from list
+    if animal_index >= 0:
+        ANIMALS.pop(animal_index)
+
+def update_animal(id, new_animal):
+    # Iterate the ANIMALS list, but use enumerate() so that
+    # you can access the index value of each item.
+    for index, animal in enumerate(ANIMALS):
+        if animal["id"] == id:
+            # Found the animal. Update the value.
+            ANIMALS[index] = new_animal
+            break
