77799b667c69cca57e4b5be94edfb35ad0e3e097
Adam Featherston <j.adam.featherston@gmail.com>
2022-10-14 11:51:12 -0500

    SQL adding and deleting

--- request_handler.py
+++ request_handler.py
@@ -34,6 +34,7 @@ from models import Animal
 from models import Customer
 from models import Employee
 from models import Location
+from urllib.parse import urlparse, parse_qs
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -45,25 +46,21 @@ class HandleRequests(BaseHTTPRequestHandler):
     """Controls the functionality of any GET, PUT, POST, DELETE requests to the server"""
 
     def parse_url(self, path):
-        # Just like splitting a string in JavaScript. If the
-        # path is "/animals/1", the resulting list will
-        # have "" at index 0, "animals" at index 1, and "1"
-        # at index 2.
-        path_params = path.split("/")
+        """Parse the url into the resource and id"""
+        parsed_url = urlparse(path)
+        path_params = parsed_url.path.split("/")  # ['', 'animals', 1]
         resource = path_params[1]
-        id = None
 
-        # Try to get the item at index 2
+        if parsed_url.query:
+            query = parse_qs(parsed_url.query)
+            return (resource, query)
+
+        pk = None
         try:
-            # Convert the string "1" to the integer 1
-            # This is the new parseInt()
-            id = int(path_params[2])
-        except IndexError:
-            pass  # No route parameter exists: /animals
-        except ValueError:
-            pass  # Request had trailing slash: /animals/
-
-        return (resource, id)  # This is a tuple
+            pk = int(path_params[2])
+        except (IndexError, ValueError):
+            pass
+        return (resource, pk)  # This is a tuple
 
     # This is a Docstring it should be at the beginning of all classes and functions
     # It gives a description of the class or function
@@ -74,50 +71,43 @@ class HandleRequests(BaseHTTPRequestHandler):
     # It handles any GET request.
     def do_GET(self):
         self._set_headers(200)
-        # Parse the URL and capture the tuple that is returned
-        (resource, id) = self.parse_url(self.path)
 
-        if resource == "animals":         
-            
-            if id is not None:
-                response = get_single_animal(id)
-                
-                if response is not None:
-                   self._set_headers(200)
+        response = {}
 
+        # Parse URL and store entire tuple in a variable
+        parsed = self.parse_url(self.path)
+
+        # If the path does not include a query parameter, continue with the original if block
+        if "?" not in self.path:
+            (resource, id) = parsed
+
+            if resource == "animals":
+                if id is not None:
+                    response = f"{get_single_animal(id)}"
                 else:
-                    self._set_headers(404)
-                    response = { "message": f"Animal {id} is out playing right now" }      
-          
-            else:
-                self._set_headers(200)
-                response = get_all_animals()
+                    response = f"{get_all_animals()}"
+            elif resource == "customers":
+                if id is not None:
+                    response = f"{get_single_customer(id)}"
+                else:
+                    response = f"{get_all_customers()}"
+            elif resource == "employees":
+                if id is not None:
+                    response = f"{get_single_employee(id)}"
+                else:
+                    response = f"{get_all_employees()}" 
+            elif resource == "locations":
+                if id is not None:
+                    response = f"{get_single_location(id)}"
+                else:
+                    response = f"{get_all_locations()}"              
 
-        else:
-            self._set_headers(404)
-            response = { "message": f"Ruh, roh: something went wrong" }  
+        else:  # There is a ? in the path, run the query param functions
+            (resource, query) = parsed
 
-        if resource == "locations":
-            self._set_headers(200)
-            if id is not None:
-                response = get_single_location(id)
-
-            else:
-                response = get_all_locations()
-
-        if resource == "customers":
-            if id is not None:
-                response = get_single_customer(id)
-
-            else:
-                response = get_all_customers()
-
-        if resource == "employees":
-            if id is not None:
-                response = get_single_employee(id)
-
-            else:
-                response = get_all_employees()
+            # see if the query dictionary has an email key
+            if query.get("email") and resource == "customers":
+                response = get_customer_by_email(query["email"][0])
 
         self.wfile.write(json.dumps(response).encode())
 
@@ -125,7 +115,7 @@ class HandleRequests(BaseHTTPRequestHandler):
     # It handles any POST request.
 
     def do_POST(self):
-        
+
         content_len = int(self.headers.get("content-length", 0))
         post_body = self.rfile.read(content_len)
 
@@ -137,7 +127,7 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         # Initialize new entry(s)
         new_entry = None
-       
+
         # Add a new animal to the list. Don't worry about
         # the orange squiggle, you'll define the create_animal
         # function next.
@@ -153,8 +143,10 @@ class HandleRequests(BaseHTTPRequestHandler):
                 new_entry = create_location(post_body)
             else:
                 self._set_headers(400)
-                new_entry = {"message": f'{"name is required" if "name" not in post_body else ""} {"address is required" if "address" not in post_body else ""}'}
-      
+                new_entry = {
+                    "message": f'{"name is required" if "name" not in post_body else ""} {"address is required" if "address" not in post_body else ""}'
+                }
+
         # Add a new employee to the list.
         if resource == "employees":
             # Initialize new location
@@ -169,7 +161,7 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.wfile.write(json.dumps(new_entry).encode())
 
     def do_DELETE(self):
-      
+
         # Parse the URL
         (resource, id) = self.parse_url(self.path)
 
@@ -181,7 +173,7 @@ class HandleRequests(BaseHTTPRequestHandler):
         # Delete a single customer from the list
         if resource == "customers":
             self._set_headers(405)
-            response = { "message": f"Ruh, roh: This function is not supported, sorry!" }
+            response = {"message": f"Ruh, roh: This function is not supported, sorry!"}
 
         # Delete a single employee from the list
         if resource == "employees":
@@ -244,8 +236,7 @@ class HandleRequests(BaseHTTPRequestHandler):
         """Sets the options headers"""
         self.send_response(200)
         self.send_header("Access-Control-Allow-Origin", "*")
-        self.send_header("Access-Control-Allow-Methods",
-                         "GET, POST, PUT, DELETE")
+        self.send_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
         self.send_header(
             "Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept"
         )
